2b8e6823dabef25ea3d46a13e717f0dd9198dae0
Andrezinrc <andrem.33333@gmail.com>
2025-06-26 02:08:59 +0000

    feat: adicionar estrutura inicial do servidor e cliente

--- client.c
+++ client.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <arpa/inet.h>
 
-int main() {
-    printf("Hello, World\n");
+#define SERVER_IP "127.0.0.1"
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main(){
+    int sockfd;
+    // socket do cliente para conexão com o servidor
+    
+    struct sockaddr_in server_addr;
+    // endereco do servidor -> IP e porta
+    
+    char buffer[BUFFER_SIZE];
+    // buffer para enviar ou receber dados
+
+    // cria socket
+    // configura servidor
+    // conecta ao servidor
+    // abre arquivo para leitura
+    // envia dados
+    // fecha conexão
+    
     return 0;
 }
--- server.c
+++ server.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
 int main() {
-    printf("Hello, World\n");
+    int server_fd, client_fd; 
+    // sockets do servidor e do cliente
+    
+    struct sockaddr_in server_addr, client_addr;
+    // enderecos do servidor e do cliente
+    
+    socklen_t client_size = sizeof(client_addr);
+    // tamanho da struct client_addr (para accept)
+    
+    char buffer[BUFFER_SIZE];
+    // buffer para armazenar dados recebidos
+    
+    // cria o socket
+    // configura o endereço do servidor
+    // associa socket a porta
+    // comeca a escutar
+    // aceita uma conexão
+    // abre arquivo para escrita
+    // recebe dados e salva
+    // fecha conexões
+    
     return 0;
 }
