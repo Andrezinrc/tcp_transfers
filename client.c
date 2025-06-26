d3a611530188cdaa844263aa583f83fa80466232
Andrezinrc <andrem.33333@gmail.com>
2025-06-26 07:46:07 +0000

    feat: implementa envio e recebimento de arquivos via TCP

--- client.c
+++ client.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 
 #define SERVER_IP "127.0.0.1"
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-int main(){
+int main(int argc, char *argv[]){
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <arquivo_para_enviar>\n", argv[0]);
+        return 1;
+    }
+    
+    const char *file = argv[1];
+    
     int sockfd;
     // socket do cliente para conexão com o servidor
     
@@ -16,11 +26,35 @@ int main(){
     // buffer para enviar ou receber dados
 
     // cria socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    
     // configura servidor
+    server_addr.sin_family = AF_INET;                     // ipv4
+    server_addr.sin_port = htons(PORT);                   // porta do servidor
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr); // ponverte ip string para formato binario
+        
     // conecta ao servidor
+    connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    printf("Conectado ao servidor!\n");
+    
     // abre arquivo para leitura
+    FILE *fp = fopen(file, "rb");
+    if (!fp) {
+        perror("Erro ao abrir arquivo");
+        close(sockfd);
+        return 1;
+    }
+    
     // envia dados
+    int bytes;
+    while ((bytes = fread(buffer, 1, BUFFER_SIZE, fp)) > 0) {
+        send(sockfd, buffer, bytes, 0);
+    }
+    printf("Arquivo enviado com sucesso!\n");
+    fclose(fp);
+    
     // fecha conexão
+    close(sockfd);
     
     return 0;
 }
--- server.c
+++ server.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h> 
 #include <arpa/inet.h>
 
 #define PORT 8080
@@ -18,13 +21,43 @@ int main() {
     // buffer para armazenar dados recebidos
     
     // cria o socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    
     // configura o endereço do servidor
+    server_addr.sin_family = AF_INET;         // ipv4
+    server_addr.sin_port = htons(PORT);       // porta do servidor
+    server_addr.sin_addr.s_addr = INADDR_ANY; // aceita conexões em qualquer interface de rede
+        
     // associa socket a porta
+    bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    
     // comeca a escutar
+    listen(server_fd, 5);
+    printf("Servidor ouvindo na porta %d...\n", PORT);
+    
     // aceita uma conexão
+    client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_size);
+    printf("Cliente conectado!\n");
+    
     // abre arquivo para escrita
+    FILE *fp = fopen("teste.txt", "wb");
+    if (!fp) {
+        perror("Erro ao abrir arquivo");
+        close(client_fd);
+        close(server_fd);
+        return 1;
+    }
+    
     // recebe dados e salva
+    int bytes;
+    while ((bytes = recv(client_fd, buffer, BUFFER_SIZE, 0)) > 0) {
+        fwrite(buffer, 1, bytes, fp);
+    }
+    
     // fecha conexões
+    close(client_fd);
+    close(server_fd);
+    return 0;
     
     return 0;
 }
